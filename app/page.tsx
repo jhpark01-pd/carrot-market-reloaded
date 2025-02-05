bc3af35a0b4600e5e5da5beec6c6c99149552cb0
Jongho Park <jhpark01@plandocs.co.kr>
2025-02-05 10:25:16 +0900

    Tailwind Variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,22 @@
 export default function Home() {
-  return <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-    <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col">
-          <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-400">In transit</span>
-          <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
-        </div>
-        <div className="bg-orange-400 size-12 rounded-full"/>
-      </div>
-      <div className="my-2 flex items-center gap-2">
-        <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:scale-125 hover:bg-green-500">Today</span>
-        <span className="dark:text-gray-100">9:30-10:30</span>
-      </div>
-      <div className="relative">
-        <div className="bg-gray-200 w-full h-2 rounded-full absolute"/>
-        <div className="bg-green-400 w-2/3 h-2 rounded-full absolute"/>
-      </div>
-      <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-        <span>Expected</span>
-        <span>Sorting center</span>
-        <span>In transit</span>
-        <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-      </div>
+  return <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2">
+      <input type="text" placeholder="Search here..." 
+      className="w-full rounded-full h-10 bg-gray-200 pl-4 outline-none transition-shadow 
+        ring ring-transparent 
+        placeholder:text-orange-500 
+        placeholder:drop-shadow
+        focus:ring-red-400 
+        focus:ring-offset-2" />
+      <button className="bg-black 
+        text-white 
+        py-2 
+        rounded-full 
+        active:scale-90 
+        transition-transform 
+        font-medium 
+        outline-none">Search
+      </button>
     </div>
   </main>
 }
